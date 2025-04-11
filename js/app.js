72bc36785576966785dbddbfc99f166da12a8895
Vitor Macedo <macedov836@gmai.com>
2025-04-11 11:34:50 -0300

    Confirmacao se quer devolver mesmo o jogo

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
 
     if(imagem.classList.contains('dashboard__item__img--rented')){
+        if(confirm(`Voce realmente quer devolver o jogo ${nomeJogo.textContent}?`));
         imagem.classList.remove('dashboard__item__img--rented')
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'alugar';
